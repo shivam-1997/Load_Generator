3404aaf1736a75a5c0375110c56bd2f72ac56f95
Shivam Poddar <38297185+shivam-1997@users.noreply.github.com>
2018-11-09 12:34:23 +0530

    1st Commmit

---
+++ multithreaded_cient.cpp
@@ -0,0 +1,285 @@
+//CLIENT
+#include <bits/stdc++.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <sys/types.h>//socket
+#include <sys/socket.h>//socket
+#include <string.h>//memset
+#include <stdlib.h>//sizeof
+#include <netinet/in.h>//INADDR_ANY
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+// #define SERVER_IP "127.0.0.1"
+#define MAXSZ 256
+// #define NUM_OF_WORKERS 4
+int NUM_OF_WORKERS;
+// int *requests;
+// double *responseTime;
+int requests[200];
+double responseTime[200];
+void error(char *msg);
+
+
+struct arg_struct {
+    int thread_id;
+};
+
+char thread_n[10];
+
+int connect_fun(char *server_ip,int server_port, int &sockfd, struct sockaddr_in &serverAddress, struct hostent *(&server)){
+    //create socket
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    //initialize the socket addresses
+    memset(&serverAddress,0,sizeof(serverAddress));
+    server = gethostbyname(server_ip);
+    if(server == NULL){
+        fprintf(stderr, "No such host\n");
+        exit(0);
+    }
+
+    serverAddress.sin_family=AF_INET;
+
+    // serverAddress.sin_addr.s_addr=inet_addr(server->);
+    bcopy((char*)server->h_addr,    (char*)&serverAddress.sin_addr.s_addr,  server->h_length);
+    serverAddress.sin_port=htons(server_port);
+    //client  connect to server on port
+    if(connect(sockfd,(struct sockaddr *)&serverAddress,sizeof(serverAddress)) != 0) return 0;
+    // cout<<"Connection request successfully sent"<<endl;
+    char msg[3];
+    bzero(msg,3);
+    int bytes_read = read(sockfd,msg,2);
+    // if(bytes_read>0)
+    // cout<<"connection established. "<<msg<<endl;
+    // else
+    // cout<<"connection not established. "<<endl;
+    return atoi(msg);
+}
+void disconnect_fun(int &sockfd){
+  int n_write = write(sockfd, "disconnect", sizeof("disconnect"));
+  char msg[MAXSZ];
+  int n_read = read(sockfd, msg, MAXSZ);
+  msg[n_read] = 0;
+  cout<<msg<<" thread "<< thread_n<<endl;
+  close(sockfd);
+  // else cout<<"Error, unable to disconnect from client side"<<endl;
+
+}
+
+double executeCommand(std::string command, int &sockfd, struct sockaddr_in &serverAddress, struct hostent *(&server)){
+    clock_t t;  // in seconds
+    struct timeval begin, end;
+    // cout<<command<<endl;
+    // Used to split string around spaces.
+    int total_length = command.length();
+    std::stringstream ss(command);
+    string operation;
+    ss>>operation;
+    int response_t = 0;
+    try {
+      if(operation.compare("connect")==0){
+
+        string server_ip_string, server_port;
+        ss>>server_ip_string;
+        ss>>server_port;
+        int n = server_ip_string.length();
+        char server_ip[n+1];
+        strcpy(server_ip, server_ip_string.c_str());
+
+        // t = clock();
+        gettimeofday(&begin, NULL);
+        if(connect_fun(server_ip, stoi(server_port), sockfd, serverAddress, server)==1){
+             // cout<<"Waiting for thread id"<<endl;
+            // t = clock() - t;
+            gettimeofday(&end, NULL);
+            int n = read(sockfd, thread_n,10);
+            cout<<"OK-connected to thread "<<thread_n<<endl;
+
+            return ((end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec)) / 1000000;
+        }
+        else perror("Unable to connect to server");
+      }
+      else if(operation.compare("disconnect")==0){
+
+          // t = clock();
+          gettimeofday(&begin, NULL);
+          disconnect_fun( sockfd);
+          // t = clock() - t;
+          gettimeofday(&end, NULL);
+          return ((end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec)) / 1000000;
+      }
+      else if((operation.compare("create")==0) || (operation.compare("delete")==0)
+            ||(operation.compare("update")==0) || (operation.compare("read")==0)){
+
+        int n = command.length();
+        char msg[MAXSZ];
+        /*writeing process starts */
+        // cout<<n<<" character command to be sent"<<endl;
+        bzero(msg,MAXSZ);
+        int temp = n;
+        strcpy(msg, command.c_str());
+        msg[MAXSZ-1]=0;
+
+        // t = clock();
+        gettimeofday(&begin, NULL);
+        // do{
+        temp -= write(sockfd, msg, MAXSZ);
+        // }while(temp>0);
+        // cout<<"Complete command with "<<n<<" characters sent."<<endl;
+        /*writeing process ends*/
+
+        /* receiving process starts */
+        bzero(msg,MAXSZ);
+        int n_transfer = read(sockfd, msg, MAXSZ);
+        // t = clock() - t;
+        gettimeofday(&end, NULL);
+        return ((end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec)) / 1000000;
+        // cout<<n_transfer<<" Bytes received"<<endl;
+        msg[n_transfer] = 0;
+        cout<<msg<<endl;
+        /*receiving process ends*/
+      }
+      else cout<<"Invalid command."<<endl;
+    }
+    catch (const char* msg){
+      cerr << msg << endl;
+    }
+    // return (end.tv_usec - begin.tv_usec) * 1000;
+}
+
+
+void handle_sigint(int sig);
+
+void* testThread( void* data){
+    // signal(SIGINT, handle_sigint);
+
+    int sockfd;//to create socket
+    struct sockaddr_in serverAddress;//client will connect on this
+    struct hostent *server;
+
+    int *thread_id = (int*) data;
+    int n = *thread_id;
+    requests[n] = 0;
+    int key;
+    string command;
+    cout<<"testing started for thread "<<n<<"\n";
+    try{
+    executeCommand("connect 127.0.0.1 5500", sockfd, serverAddress, server);
+    requests[n]++;
+    while(1){
+
+        key = rand() % 10000;
+        command = "create "+ to_string(key) + " 10 abcdefghij";
+        responseTime[n] += executeCommand( command, sockfd, serverAddress, server);
+        requests[n]++;
+
+        key = rand() % 10000;
+        command = "read "+ to_string(key);
+        responseTime[n] += executeCommand( command, sockfd, serverAddress, server);
+        requests[n]++;
+
+        key = rand() % 10000;
+        command = "update " + to_string(key) + " 10 abcdefghij";
+        responseTime[n] += executeCommand( command, sockfd, serverAddress, server);
+        requests[n]++;
+
+        key = rand() % 10000;
+        command = "delete "+ to_string(key);
+        responseTime[n] += executeCommand( command, sockfd, serverAddress, server);
+        requests[n]++;
+
+    }
+    // cout<<endl;
+    executeCommand("disconnect", sockfd, serverAddress, server);
+    requests[n]++;
+}
+    catch(const char *msg){
+        cout<<"Error inside thread "<<n<<endl;
+    }
+}
+
+int calTotalRequests(int *requests, int NUM_OF_WORKERS);
+double evaluateThroughput( int totalRequests, int time);
+double evaluateResponseTime( double *responseTime, int totalRequests, int NUM_OF_WORKERS);
+
+int main(int argc, char *argv[]){
+    cout<<"Enter the number of threads: ";
+    cin>>NUM_OF_WORKERS;
+    // requests = (int*)malloc( sizeof(int) * NUM_OF_WORKERS);
+    // responseTime = (double*)malloc( sizeof(double) * NUM_OF_WORKERS);
+    // NUM_OF_WORKERS = argv[0];
+    int timeSec;
+    cout<<"Enter the time in seconds: ";
+    cin>>timeSec;
+    // timeSec = argv[1];
+    int worker_thread_id[NUM_OF_WORKERS]={0};
+    for(int i = 0; i < NUM_OF_WORKERS; i++){
+        worker_thread_id[i] = i;
+    }
+
+    pthread_t worker_thread[NUM_OF_WORKERS];
+
+    try{
+        for(int i = 0; i < NUM_OF_WORKERS; i++){
+            cout<<i<<endl;
+            pthread_create(&worker_thread[i], NULL, testThread, (void*)&worker_thread_id[i]);
+        }
+    }catch(const char* msg){
+        // cerr << msg << endl;
+        cout<<"Error in creating thread"<<endl;
+    }
+
+    int timeMilli = timeSec*1000;
+    sleep(timeSec);
+
+    cout<<"Time completed."<<endl;
+    // free(responseTime);
+    // free(requests);
+
+    // pthread_cancel(master_thread,NULL);
+    for(int i=0;i<NUM_OF_WORKERS;i++){
+        try{
+            pthread_cancel(worker_thread[i]);
+        }
+        catch(const char* msg){
+            // cerr << msg << endl;
+            cout<<"Error in killing thread"<<endl;
+        }
+    }
+    cout<<"*************************************************"<<endl;
+    int totalRequests = calTotalRequests(requests, NUM_OF_WORKERS);
+    cout<<"Total requests generated: "<< totalRequests<<endl;
+    cout<<"Throughput is "<<evaluateThroughput( totalRequests, timeSec)<<" req/sec"<<endl;
+    cout<<"Average response time is "<<evaluateResponseTime( responseTime, totalRequests, NUM_OF_WORKERS)<<endl;
+    cout<<"*************************************************"<<endl;
+    printf("Program ended\n");
+    return 0;
+}
+
+int calTotalRequests(int *requests, int NUM_OF_WORKERS){
+    int sum = 0;
+    for ( int i = 0; i< NUM_OF_WORKERS; i++){
+        sum += requests[i];
+    }
+    return sum;
+}
+double evaluateThroughput( int totalRequests, int time){
+    return double( totalRequests/ time);
+}
+double evaluateResponseTime( double *responseTime, int totalRequests, int NUM_OF_WORKERS){
+    double sum = 0.0;
+    for ( int i = 0; i< NUM_OF_WORKERS; i++){
+        sum += responseTime[i];
+    }
+    return double(sum/totalRequests);
+}
+void error(char *msg){
+    perror(msg);
+    exit(0);
+}
---
+++ server.cpp
@@ -0,0 +1,257 @@
+#include <sys/types.h>//socket
+#include <sys/socket.h>//socket
+#include <sys/wait.h>
+#include <netinet/in.h>//INADDR_ANY
+#include <arpa/inet.h>
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+using namespace std;
+
+#define NUM_OF_WORKERS 200
+#define MAXSZ 256
+
+// shared variables
+map <int, string> keyValue;        // empty map container
+int cnt = 0;
+int worker_thread_id[NUM_OF_WORKERS]={0};
+pthread_t worker_thread[NUM_OF_WORKERS];
+int ret;
+// locks and condition variables
+pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct arg_struct {
+    int arg1;
+    int arg2;
+};
+struct arg_struct args[NUM_OF_WORKERS];
+void error(char *msg);
+
+int sockfd;//to create socket
+struct sockaddr_in clientAddress;//server writes to client on this address
+socklen_t clientAddressLength = sizeof(clientAddress);
+
+void setup_server_sock(int argc, char *argv[]);
+
+void process(char *str,int thread_n, int newsockfd){
+    string command = string(str);
+    // cout<<command<<"\n";
+    stringstream ss(command);
+    string operation;
+    ss>>operation;
+    int key;
+    ss>>key;
+
+    // pthread_mutex_lock(&my_mutex);
+    map<int, string>::iterator it = keyValue.find(key);
+    int n;
+    if(operation.compare("create")==0){
+        // cout<<"Thread_"<<thread_n<<" "<<str<<endl;
+        int value_size;
+        string value;
+        ss>>value_size;
+        ss>>value;
+        if(it != keyValue.end()){   //element found;
+            n = write(newsockfd,"Key-Value already present.", sizeof("Key-Value already present."));
+            // cout<<n<<"bytes sent"<<endl;
+            return;
+        }
+
+        keyValue.insert(pair <int, string> (key, value));
+        n = write(newsockfd,"OK", sizeof("OK"));
+        // cout<<n<<"bytes sent"<<endl;
+        // pthread_mutex_unlock(&my_mutex);
+        return;
+    }
+    if(operation.compare("read")==0){
+        // cout<<"Thread_"<<thread_n<<" "<<str<<endl;
+        // // cout<<"key----------------->"<<key<<endl;
+        if(it == keyValue.end())
+        {   //element found;
+            n = write(newsockfd,"No value for this key exists.", sizeof("No value for this key exists."));
+            // cout<<n<<"bytes sent"<<endl;
+            return;
+        }
+        string msg = string(it->second);
+        n = write(newsockfd,msg.c_str(), msg.length());
+        // cout<<n<<" bytes sent"<<endl;
+        return;
+    }
+    if(operation.compare("update")==0){
+        // pthread_mutex_lock(&my_mutex);
+        if(it == keyValue.end()){
+            n = write(newsockfd,"No value for this key exists.", sizeof("No value for this key exists."));
+            // cout<<n<<"bytes sent"<<endl;
+            return;
+        }
+
+        // cout<<"Thread_"<<thread_n<<" "<<str<<endl;
+        int value_size;//= atoi(strtok(NULL, " "));
+        string value;// = strtok(NULL, " ");
+        ss>>value_size;
+        ss>>value;
+
+        keyValue[key]=value;
+        n = write(newsockfd, "OK", sizeof("OK"));
+        // cout<<n<<"bytes sent"<<endl;
+        // // cout<<"updated"<<endl;
+        // pthread_mutex_unlock(&my_mutex);
+        return;
+    }
+
+    if(operation.compare("delete")==0){
+        // pthread_mutex_lock(&my_mutex);
+        // cout<<"Thread_"<<thread_n<<" "<<str<<endl;
+        // // cout<<"key----------------->"<<key<<endl;
+        if(it == keyValue.end()){
+            //element not found;
+            n = write(newsockfd,"No value for this key exists.", sizeof("No value for this key exists."));
+            // cout<<n<<"bytes sent"<<endl;
+            return;
+        }
+        int num = keyValue.erase(key);
+        if(num<1) {
+            n = write(newsockfd, "Unable to delete", sizeof("Unable to delete"));
+            // cout<<n<<"bytes sent"<<endl;
+        }
+        else {
+            n = write(newsockfd, "OK", sizeof("OK"));
+            // cout<<n<<"bytes sent"<<endl;
+        }
+        // pthread_mutex_unlock(&my_mutex);
+    }
+    return;
+}
+
+void* thread_fun(void*(data)){
+    // pthread_mutex_unlock(&my_mutex);
+    // pthread_detach(pthread_self());
+    struct arg_struct *args = (struct arg_struct *)data;
+    int newsockfd = args->arg1;
+    int thread_n = args->arg2;
+    printf("thread_%d connected to client: %s\n",thread_n, inet_ntoa(clientAddress.sin_addr));
+    char str[10];
+    sprintf(str, "%d", thread_n);
+    int bytes_trans = write(newsockfd, str, 10);
+    // // cout<<bytes_trans<<"bytes sent"<<endl;
+    while(1){
+
+        char msg[MAXSZ];
+        bzero(msg,MAXSZ);
+        int n=read(newsockfd,msg,MAXSZ);
+        if(n<=1)  continue;
+        // cout<<n<<"bytes received"<<endl;
+
+        pthread_mutex_lock(&my_mutex);
+        if(strcmp(msg,"disconnect")==0){
+            // pthread_mutex_lock(&my_mutex);
+            cnt--;
+            int n_write = write(newsockfd,"OK:disconnected from server", sizeof("OK:disconnected from server"));
+            // cout<<n_write<<"bytes sent"<<endl;
+            close(newsockfd);
+            // cout<<"Thread_"<<thread_n<<" disconnected"<<endl;
+            worker_thread_id[thread_n] = 0;
+            pthread_mutex_unlock(&my_mutex);
+            // return NULL;
+            pthread_exit(NULL);
+        }
+        else {
+            process(msg, thread_n, newsockfd);
+        }
+        pthread_mutex_unlock(&my_mutex);
+    }
+}
+
+void* master_thread_fun(void *){
+    int newsockfd[NUM_OF_WORKERS] = {0};
+    int index=0;
+
+    cout<<"Server is open to accept "<<NUM_OF_WORKERS<<" connections.\n";
+    while(1){
+
+        cout<<index<<endl;
+        newsockfd[index] = accept(sockfd,(struct sockaddr*)&clientAddress,&clientAddressLength);
+        if(newsockfd[index]>0){
+            pthread_mutex_lock(&my_mutex);
+            // cout<<"New request arrived";
+            if(cnt<NUM_OF_WORKERS){
+                int bytes_trans = write(newsockfd[index],"1",2); // 1 = successfully connected
+                // cout<<bytes_trans<<"bytes sent"<<endl;
+                cnt++;
+                int i;
+                // create worker threads
+                // for(i = 0; i < NUM_OF_WORKERS; i++){
+                    // if(worker_thread_id[i]==0){
+                        worker_thread_id[index]=1;
+
+                        args[index].arg1 = newsockfd[index];
+                        args[index].arg2 = index;
+                        // printf("index %d\n", args[index].arg2);
+
+                        // cout<<"Allocated to thread "<<i<<endl;
+                        pthread_create(&worker_thread[index], NULL, thread_fun, (void*)&args[index]);
+                        // pthread_mutex_unlock(&my_mutex);
+                        //break;
+                    //}
+                //}
+                // cout<<"Returned to listening\n";
+            }
+            else{
+                int bytes_trans = write(newsockfd[index],"0",2);
+                // cout<<bytes_trans<<"bytes sent"<<endl;
+                if(close(newsockfd[index])==0)  cout<<"Can't accept anymore connections.\n";
+            }
+            index++;
+            pthread_mutex_unlock(&my_mutex);
+        }
+
+        for(int i=0;i<NUM_OF_WORKERS;i++){
+          // pthread_cond_broadcast(&empty);
+          // pthread_join(worker_thread[i],NULL);
+        }
+    }
+}
+int main(int argc, char *argv[]){
+    setup_server_sock(argc, argv);
+    //cout<<"enter the number of threads to be spawned:\t";
+    int master_thread_id = 0;
+    pthread_t master_thread;
+    //create master thread
+    pthread_create(&master_thread, NULL, master_thread_fun, (void *)&master_thread_id);
+    pthread_join(master_thread, NULL);
+    printf("Program ended\n");
+    return 0;
+}
+
+//below are the final functions
+void error(char *msg){
+    perror(msg);
+    exit(1);
+}
+
+void setup_server_sock(int argc, char *argv[]){
+    if (argc < 3) {
+		fprintf(stderr,"ERROR, too low arguments\n");
+		exit(1);
+	}
+    //create socket
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd<0) error("unable to create server socket");
+    int opt=1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        error("setsockopt");
+    }
+    //initialize the socket addresses
+    struct sockaddr_in serverAddress;//server receive on this address
+    bzero((char*)&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family=AF_INET;
+    serverAddress.sin_addr.s_addr= inet_addr(argv[1]);
+    // serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+    serverAddress.sin_port=htons(atoi(argv[2]));
+    //bind the socket with the server address and port
+    if(bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress))<0) error("error on binding");
+    //listen for connection from client
+    listen(sockfd,NUM_OF_WORKERS+1);
+}
